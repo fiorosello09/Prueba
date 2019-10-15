9dbe643f07d4f7c027374bd495e7cb53b8d6fc91
fiorosello09 <fiorosello09@hotmail.com>
2019-10-15 15:47:16 -0600

    Remove comments

--- src/Datos/ListaSimple.java
+++ src/Datos/ListaSimple.java
@@ -5,19 +5,6 @@
  */
 package Datos;
 
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author fioro
- * @param <L>
- */
 public class ListaSimple<L>
 {
     
