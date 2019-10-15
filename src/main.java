143157f0f2d1c82df94366fe44c74a2e1c194a7c
fiorosello09 <fiorosello09@hotmail.com>
2019-10-15 15:34:59 -0600

    First commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+import Ventanas.VentanaPrincipal;
+
+
+public class main {
+    public static void main(String[] args) 
+    { 
+       
+     VentanaPrincipal hola = new VentanaPrincipal();
+     hola.setVisible(true);
+     
+        
+    }
+}
